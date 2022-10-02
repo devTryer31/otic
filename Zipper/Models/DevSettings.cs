a2a60d3abdb3fb99fa841a6f19f8aeea229ee969
Валера Герасимов <valera.gs@bk.ru>
2022-10-02 13:09:17 +0300

    Заготовки для настроек.

---
+++ Zipper/Models/DevSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zipper.Models
+{
+    [Serializable]
+    public class DevSettings
+    {
+        public string? DefaultFolderToEncodePath { get; set; }
+
+        public string? DefaultFolderToDecodePath { get; set; }
+
+        public string? DefaultFaacFilePath { get; set; }
+    }
+}
---
+++ Zipper/ViewModels/DevSettingsViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Zipper.ViewModels
+{
+    public class DevSettingsViewModel : ViewModelBase
+    {
+        private string? _defaultFolderToEncodePath;
+
+        public string? DefaultFolderToEncodePath
+        {
+            get => _defaultFolderToEncodePath;
+            set => Set(ref _defaultFolderToEncodePath, value);
+        }
+
+        private string? _defaultFolderToDecodePath;
+
+        public string? DefaultFolderToDecodePath
+        {
+            get => _defaultFolderToDecodePath;
+            set => Set(ref _defaultFolderToDecodePath, value);
+        }
+
+        private string? _defaultFaacFilePath;
+
+        public string? DefaultFaacFilePath
+        {
+            get => _defaultFaacFilePath;
+            set => Set(ref _defaultFaacFilePath, value);
+        }
+    }
+}
